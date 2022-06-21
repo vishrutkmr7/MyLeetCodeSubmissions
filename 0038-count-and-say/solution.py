ec4ae733d5be5d2fa5416053db071db5e31d6184
Vishrut Jha <vishrutkmr7@gmail.com>
2022-06-21 08:44:14 +0000

    [LeetCode Sync] Runtime - 85 ms (6.39%), Memory - 13.9 MB (100.00%)

---
+++ 0038-count-and-say/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    
+    def say(self, n:str) -> str:
+        res = ""
+        stack = []
+        for digit in map(int, n):
+            if len(stack) == 0:
+                stack.append(digit)
+                continue
+
+            if stack[-1] != digit:
+                res += str(len(stack)) + str(stack[-1])
+                stack = []
+                
+            stack.append(digit)
+                
+        return res + str(len(stack)) + str(stack[-1])
+        
+        
+    def countAndSay(self, n: int) -> str:
+        # Base
+        if n == 1:
+            return "1"
+        said = "1"
+        for i in range(1, n):
+            said = self.say(said)
+        
+        return said
+            
+        
+        
