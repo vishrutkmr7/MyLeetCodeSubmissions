ed7d9371a3013304464a00bdffe60ee5af792bde
Vishrut Jha <vishrutkmr7@gmail.com>
2022-06-26 22:45:31 +0000

    [LeetCode Sync] Runtime - 72 ms (5.23%), Memory - 14.3 MB (100.00%)

---
+++ 0205-isomorphic-strings/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        marked = [False] * 256
+        mapr = [-1] * 256
+        
+        for i in range(len(s)):
+            if mapr[ord(s[i])] == -1:
+                if marked[ord(t[i])] == True:
+                    return False
+                marked[ord(t[i])] = True
+                mapr[ord(s[i])] = t[i]
+            elif mapr[ord(s[i])] != t[i]:
+                return False
+
+        return True
+    
