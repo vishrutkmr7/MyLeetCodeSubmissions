6929a996d2434333caee61f4320041e5908d853f
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-06 01:19:54 +0000

    Sync LeetCode submission - Two Sum (python3)

---
+++ problems/two_sum/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = defaultdict(int)
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hashMap:
+                return[i, hashMap[diff]]
+            hashMap[nums[i]] = i
