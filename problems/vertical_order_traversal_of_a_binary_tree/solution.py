f30948ba3813ae5ffc8cdf48bbd8ef137930d66d
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-07 19:21:55 +0000

    Sync LeetCode submission - Vertical Order Traversal of a Binary Tree (python3)

---
+++ problems/vertical_order_traversal_of_a_binary_tree/solution.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import heapq as hq
+
+
+class Solution:
+        
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        heap = []
+        hq.heapify(heap)
+        
+        def dfs(root, depth, position):
+            if not root:
+                return
+            else:
+                hq.heappush(heap, (position, depth, root.val))
+                dfs(root.left, depth + 1, position - 1)
+                dfs(root.right, depth + 1, position + 1)
+                
+        dfs(root, 0, 0)
+        result = []
+        posHold = -float("inf")
+        arrayHold = []
+        while heap:
+            pos, depth, val = hq.heappop(heap)
+            # print((pos, depth, val))
+            if pos != posHold:
+                if len(arrayHold) > 0:
+                    result.append(arrayHold)
+                    arrayHold = []
+                posHold = pos
+            arrayHold.append(val)
+        result.append(arrayHold)
+        return result
