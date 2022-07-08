12ced8754fd900874f6b46fd663782cafea85836
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-08 07:54:48 +0000

    [LeetCode Sync] Runtime - 46 ms (6.88%), Memory - 14 MB (100.00%)

---
+++ 1127-last-stone-weight/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 0:
+            return 0
+        
+        while len(stones) > 1:
+            stones = sorted(stones, reverse=True)
+            stone1 = stones.pop(0)
+            stone2 = stones.pop(0)
+            # if stone1 - stone2 != 0:
+            stones.append(stone1 - stone2)
+                
+        return stones[0]
