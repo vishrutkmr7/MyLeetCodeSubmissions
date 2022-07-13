2a4daf91017f29362a34bed4c5b03eef93faa4a8
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-13 00:25:56 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (python3)

--- problems/binary_tree_level_order_traversal/solution.py
+++ problems/binary_tree_level_order_traversal/solution.py
@@ -19,11 +19,14 @@ class Solution:
             if node.left:
                 queue.append((node.left, level+1))
             if node.right:
-                queue.append((node.right, level+1))
-            if result and total_level < level or not result:
+                queue.append((node.right, level+1)) 
+            if not result:
                 result.append([node.val])
             else:
-                result[-1].append(node.val)
+                if total_level < level:
+                    result.append([node.val])
+                else:
+                    result[-1].append(node.val)
             total_level = max(level, total_level)
         return result
         
