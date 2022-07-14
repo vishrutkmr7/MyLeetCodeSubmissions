f201fdb42550e09da44f73dc49280a99f75adf42
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-14 06:46:58 +0000

    [LeetCode Sync] Runtime - 200 ms (5.18%), Memory - 14.7 MB (100.00%)

---
+++ 0967-minimum-falling-path-sum/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        result =[]
+        col = len(matrix)
+        row = len(matrix[0])
+        dp = [[i for i in range(col)] for j in range(row)]
+        for j in range(row):
+                dp[0][j] = matrix[0][j]
+        
+        for i in range(1, col):
+            for j in range(0, row):
+                if j == 0:
+                    dp[i][j] = matrix[i][j] + min(dp[i - 1][j], dp[i - 1][j + 1])
+                elif j == row - 1:
+                    dp[i][j] = matrix[i][j] + min(dp[i - 1][j - 1],dp[i - 1][j])
+                else:
+                    dp[i][j] = matrix[i][j] + min(dp[i - 1][j - 1], dp [i - 1][j], dp[i - 1][j + 1])
+        
+        return min(dp[col - 1])
