0ab6a79d36892117049b3f66dd3521b606b0bf4a
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-03 03:27:44 +0000

    [LeetCode Sync] Runtime - 38 ms (39.98%), Memory - 14 MB (100.00%)

--- 0062-unique-paths/solution.py
+++ 0062-unique-paths/solution.py
@@ -1,16 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 0 or n == 0:
-            return 0
-        dp = [[0] * m for _ in range(n)]
+        dp = [[0] * n for _ in range(m)]
         for i in range(m):
-            dp[0][i] = 1
-
+            dp[i][0] = 1
         for j in range(n):
-            dp[j][0] = 1
-
-        for i in range(1, n):
-            for j in range(1, m):
+            dp[0][j] = 1
+        for i in range(1, m):
+            for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-    
-        return dp[n - 1][m - 1]
+        return dp[-1][-1]
