cc6dd6bd2beef59c80267ada4300f17f508396e2
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-03 03:32:16 +0000

    Sync LeetCode submission - My Calendar I (python3)

---
+++ problems/my_calendar_i/solution.py
@@ -0,0 +1,25 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.booked = []
+    
+    def check_booking(self, start, end):
+        for x, y in self.booked:
+            
+            if (start >= x and start < y) or (end > x and end <= y):    
+                return False
+            
+            if x > start and y < end:
+                return False
+        return True
+
+    def book(self, start: int, end: int) -> bool:
+        if self.check_booking(start, end):
+            self.booked.append([start, end])
+            return True
+        return False
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
