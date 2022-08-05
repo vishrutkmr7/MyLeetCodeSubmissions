daaa647c173fe02c8e7ce9a6eaf7f3ef8bc0082a
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-05 07:35:56 +0000

    [LeetCode Sync] Runtime - 352 ms (5.41%), Memory - 15.2 MB (100.00%)

---
+++ 0581-shortest-unsorted-continuous-subarray/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        
+        monotonic_stack = []
+        indexes = []
+        begin = None
+        end = None
+        cur_max = None
+        
+        for i in range(n):
+            while len(monotonic_stack) > 0 and nums[i] < monotonic_stack[-1]:
+                val = monotonic_stack.pop()
+                ind = indexes.pop()
+                if begin is None or ind < begin:
+                    begin = ind
+                if cur_max is None or val > cur_max:
+                    cur_max = val
+                if end is None or i > end:
+                    end = i
+            if cur_max is not None and nums[i] < cur_max:
+                end = i
+            monotonic_stack.append(nums[i])
+            indexes.append(i)
+        
+        if begin is not None:
+            return end - begin + 1
+        else:
+            return 0
