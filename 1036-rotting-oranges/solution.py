dee663be3bee31e17a6bd866f82176ad512d0ef0
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-06 03:43:31 +0000

    [LeetCode Sync] Runtime - 90 ms (5.01%), Memory - 13.9 MB (100.00%)

---
+++ 1036-rotting-oranges/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        q = []
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
+        time = 0
+        fresh = 0
+        vis = [[True] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    q.append([i, j])
+                elif grid[i][j] == 1:
+                    fresh += 1
+        while q:
+            for _ in range(len(q)):
+                x, y = q.pop(0)
+                for k in range(4):
+                    if x + dx[k] > -1 and x + dx[k] < m and y + dy[k] > - 1 and y + dy[k] < n:
+                        if grid[x + dx[k]][y + dy[k]] == 1:
+                            grid[x + dx[k]][y + dy[k]] = 2
+                            q.append([x + dx[k], y + dy[k]])
+                            fresh -= 1
+            if q:
+                time += 1
+        return time if fresh == 0 else -1
