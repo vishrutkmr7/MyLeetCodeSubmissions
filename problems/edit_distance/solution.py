a762cb086b41c88322e4731c618c46c9c0141312
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-06 20:33:17 +0000

    Sync LeetCode submission - Edit Distance (python3)

---
+++ problems/edit_distance/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        
+        @cache
+        def dp(i, j):
+            if i == 0:
+                return j
+            if j == 0:
+                return i
+            
+            l1, l2 = word1[i - 1], word2[j - 1]
+            if l1 != l2:
+                return 1 + min(
+                    dp(i, j - 1),
+                    dp(i - 1, j),
+                    dp(i - 1, j - 1)
+                )
+            return dp(i - 1, j - 1)         
+                    
+        return dp(m, n)
