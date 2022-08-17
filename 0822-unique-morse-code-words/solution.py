d057366da6f56b765f674c50a4167f0c64713555
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-17 19:14:32 +0000

    [LeetCode Sync] Runtime - 64 ms (7.21%), Memory - 13.8 MB (100.00%)

---
+++ 0822-unique-morse-code-words/solution.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: list[str]) -> int:
+        ENG_to_MORSE = {
+            "a": ".-",
+            "b": "-...",
+            "c": "-.-.",
+            "d": "-..",
+            "e": ".",
+            "f": "..-.",
+            "g": "--.",
+            "h": "....",
+            "i": "..",
+            "j": ".---",
+            "k": "-.-",
+            "l": ".-..",
+            "m": "--",
+            "n": "-.",
+            "o": "---",
+            "p": ".--.",
+            "q": "--.-",
+            "r": ".-.",
+            "s": "...",
+            "t": "-",
+            "u": "..-",
+            "v": "...-",
+            "w": ".--",
+            "x": "-..-",
+            "y": "-.--",
+            "z": "--..",
+        }
+        transform = lambda c: ENG_to_MORSE[c]
+        return len(Counter("".join(map(transform, word)) for word in words))
+
