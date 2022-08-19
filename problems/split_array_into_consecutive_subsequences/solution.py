326bc8dd6a3ebb839b3ce78eca126d180297bfbf
Vishrut Jha <vishrutkmr7@gmail.com>
2022-08-19 02:05:59 +0000

    Sync LeetCode submission - Split Array into Consecutive Subsequences (python3)

---
+++ problems/split_array_into_consecutive_subsequences/solution.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution:
+    def isPossible(self, nums: list[int]) -> bool:
+        counter = Counter(nums)
+        for i in sorted(counter.keys()):
+            while counter[i] > 0:
+                last = 0
+                j = i
+                k = 0
+                while counter[j] >= last:
+                    last = counter[j]
+                    counter[j] -= 1
+                    j += 1
+                    k += 1
+                if k < 3:
+                    return False
+        return True
+
