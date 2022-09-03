43b6c83f25044f012ac9958e6be904077cf817e5
Vishrut Jha <vishrutkmr7@gmail.com>
2022-09-03 22:09:39 +0000

    Sync LeetCode submission - Sort Even and Odd Indices Independently (python3)

---
+++ problems/sort_even_and_odd_indices_independently/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortEvenOdd(self, nums: List[int]) -> List[int]:
+        even = [nums[i] for i in range(0 , len(nums)) if i % 2 == 0]
+        odd = [nums[i] for i in range(0 , len(nums)) if i % 2 != 0]
+
+        even.sort()
+        odd = sorted(odd , reverse = True)
+        return [even.pop(0) if i % 2 == 0 else odd.pop(0) for i in range(len(nums))]
