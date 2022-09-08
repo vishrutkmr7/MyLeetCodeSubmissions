3eb7bad251bab94e4f59bb5073d4f03b85d97643
Vishrut Jha <vishrutkmr7@gmail.com>
2022-09-08 23:12:36 +0000

    Sync LeetCode submission - Reverse Integer (python3)

--- problems/reverse_integer/solution.py
+++ problems/reverse_integer/solution.py
@@ -1,14 +1,5 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < -1 * (2)**31 or x > 2**31 - 1:
-            return 0
-
-        s = ""
-        x_s = str(x)
         if x < 0:
-            x_s = x_s[1:]
-        for idx, i in enumerate(x_s):
-            if s and idx >= 9 and int(s) >= 147483647 and idx <= len(x_s) - 1 and int(x_s[idx])>= 2:
-                return 0
-            s = i + s
-        return -1 * int(s) if x < 0 else int(s)
+            return -int(str(-x)[::-1]) if -int(str(-x)[::-1]) >= -(2**31) else 0
+        return int(str(x)[::-1]) if int(str(x)[::-1]) <= 2**31 - 1 else 0
