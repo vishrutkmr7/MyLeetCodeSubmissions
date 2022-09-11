c00f1dc6a37a05d5a9c0dddef128861dc0c2da66
Vishrut Jha <vishrutkmr7@gmail.com>
2022-09-11 21:30:56 +0000

    [LeetCode Sync] Runtime - 1393 ms (5.01%), Memory - 27.2 MB (98.68%)

---
+++ 0219-contains-duplicate-ii/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        d = {}
+        for i, num in enumerate(nums):
+            if num in d and i - d[num] <= k:
+                return True
+            d[num] = i
+        return False
