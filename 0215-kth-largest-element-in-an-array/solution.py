a78475f64e1d3a586ea7d7c5693989d97a925f45
Vishrut Jha <vishrutkmr7@gmail.com>
2022-09-16 19:18:21 +0000

    [LeetCode Sync] Runtime - 472 ms (58.59%), Memory - 27.1 MB (99.75%)

--- 0215-kth-largest-element-in-an-array/solution.py
+++ 0215-kth-largest-element-in-an-array/solution.py
@@ -1,14 +1,5 @@
 class Solution:
-    def merge_sort(self, arr) -> List[int]:
-        if len(arr) <= 1:
-            return arr
-        mid = len(arr) // 2
-        left = self.merge_sort(arr[:mid])
-        right = self.merge_sort(arr[mid:])
-        return list(merge(left, right))
-    
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # return self.merge_sort(nums)[-k]
         return sorted(nums)[-k]
         
         
