2cad17bc72c4457c1929f2f58eb83049607fb754
Vishrut Jha <vishrutkmr7@gmail.com>
2022-09-26 21:37:35 +0000

    [LeetCode Sync] Runtime - 167 ms (5.27%), Memory - 14 MB (100.00%)

--- 0002-add-two-numbers/solution.py
+++ 0002-add-two-numbers/solution.py
@@ -3,17 +3,14 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import itertools
 
 class Solution(object):
-        
     def addTwoNumbers(self, l1, l2):
-        c=0
-        s=0
+        s = 0
         l = ListNode()
         n = l
-        s1 = ''
-        s2 = ''
+        s1 = ""
+        s2 = ""
         while l1:
             s1 += str(l1.val)
             l1 = l1.next
@@ -24,7 +21,6 @@ class Solution(object):
         t = 1
         for i in str(s)[::-1]:
             n.val = int(i)
-            print(t)
             if t == len(str(s)):
                 break
             t += 1
