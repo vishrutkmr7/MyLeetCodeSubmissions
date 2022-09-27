de941ffce2d5685dbb2d63fb1e4b55b484fbb346
Vishrut Jha <vishrutkmr7@gmail.com>
2022-09-27 20:30:21 +0000

    Sync LeetCode submission - Binary Tree Pruning (python3)

--- problems/binary_tree_pruning/solution.py
+++ problems/binary_tree_pruning/solution.py
@@ -6,16 +6,10 @@
 #         self.right = right
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
+        if not root:
             return None
-        
-        if self.pruneTree(root.left) is None:
-            root.left = None
-        if self.pruneTree(root.right) is None:
-            root.right = None
-        
-        if root.val != 1 and root.left is None and root.right is None:
-            root = None
-        
-        return root
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        return None if root.val == 0 and not root.left and not root.right else root
+
         
