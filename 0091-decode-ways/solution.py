bd6b0ff392bd2bdf0dbb01c2ccd36bf0da41fc3d
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-01 08:16:28 +0000

    [LeetCode Sync] Runtime - 40 ms (29.24%), Memory - 14.2 MB (100.00%)

--- 0091-decode-ways/solution.py
+++ 0091-decode-ways/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == "0":
-            return 0
-        dp = [0] * (len(s) + 1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2, len(s) + 1):
-            if s[i - 1] != "0":
-                dp[i] += dp[i - 1]
-            if s[i - 2] == "1" or (s[i - 2] == "2" and s[i - 1] <= "6"):
-                dp[i] += dp[i - 2]
-        return dp[-1]
+        dp={}
+        def check(idx):
+            if idx<len(s) and s[idx]=="0":
+                return 0
+            if idx>=len(s)-1:
+                return 1
+            if idx in dp:
+                return dp[idx]
+            if idx+1< len(s) and int(s[idx]+s[idx+1])<=26:
+                dp[idx]=check(idx+1)+check(idx+2)
+            else:
+                dp[idx]=check(idx+1)
+            return dp[idx]
+        return check(0)
