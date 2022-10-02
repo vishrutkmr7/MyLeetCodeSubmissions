c3c3de243a97d47c4de2830a1c16ba68b1d1e879
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-02 10:12:14 +0000

    [LeetCode Sync] Runtime - 1566 ms (5.14%), Memory - 19.5 MB (44.69%)

---
+++ 1263-number-of-dice-rolls-with-target-sum/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        MOD = 10**9 + 7
+        cache = {}
+
+        def dp(left, target):
+            if target < 0:
+                return 0
+            if left == 0:
+                return 1 if target == 0 else 0
+            if (left, target) in cache:
+                return cache[(left, target)]
+
+            ways = 0
+            for i in range(1, k + 1):
+                ways += dp(left - 1, target - i)
+                ways %= MOD
+            cache[(left, target)] = ways
+            return ways
+
+        return dp(n, target)
