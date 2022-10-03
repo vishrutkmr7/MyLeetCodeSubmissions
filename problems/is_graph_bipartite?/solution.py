c63039b39dedc36ce585d0f9b1988039e5b46dd9
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-03 07:00:52 +0000

    Sync LeetCode submission - Is Graph Bipartite? (python3)

---
+++ problems/is_graph_bipartite?/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isBipartite(self, arr: List[List[int]]) -> bool:
+        def dfs(v,clr,color,arr):
+
+            color[v]=clr
+
+            for i in arr[v]:
+                if color[i]==-1:
+                    if color[v]==1:
+                        if dfs(i,0,color,arr)== False:
+                            return False
+                    else:
+                        if dfs(i,1,color,arr)== False:
+                            return False
+                elif color[i]==color[v]:
+                    return False
+            return True
+        n=len(arr)
+        color=[-1]*n
+        for i in range(n):
+            if color[i]==-1:
+                if dfs(i,0,color,arr)==False:
+                    return 0
+        return 1
