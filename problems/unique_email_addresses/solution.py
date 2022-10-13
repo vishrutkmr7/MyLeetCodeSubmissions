eef69cbbbfb317066dd54dfa2fdcdc27f875a191
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-13 23:13:54 +0000

    Sync LeetCode submission - Unique Email Addresses (python3)

---
+++ problems/unique_email_addresses/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique_emails = set()
+        for email in emails:
+            local, domain = email.split("@")
+            local = local.split("+")[0].replace(".", "")
+            unique_emails.add(f"{local}@{domain}")
+        return len(unique_emails)
