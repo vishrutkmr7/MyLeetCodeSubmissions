0148a7102d9b861c843f7694837a1fea2b1c16d4
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-14 22:01:00 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- problems/rotate_image/solution.py
+++ problems/rotate_image/solution.py
@@ -1,22 +1,8 @@
 class Solution:
-    def rotate(self, matrix: list[list[int]]) -> None:
+
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
-        def rotateImg(i, k):
-            for j in range(k):
-                t = matrix[i][i + j]
-                matrix[i][i + j] = matrix[n - i - 1 - j][i]
-                matrix[n - i - 1 - j][i] = matrix[n - i - 1][n - i - 1 - j]
-                matrix[n - i - 1][n - i - 1 - j] = matrix[i + j][n - i - 1]
-                matrix[i + j][n - i - 1] = t
-
-        i = 0
-        n = len(matrix)
-        k = n - 1
-        while k > 0:
-            rotateImg(i, k)
-            i += 1
-            k -= 2
-        return
+        matrix[:] = [list(row) for row in zip(*matrix[::-1])]
+        
