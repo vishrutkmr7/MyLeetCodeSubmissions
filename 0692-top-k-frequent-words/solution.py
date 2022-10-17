972db3b64d0ede6af0203825fdb1f2a22224aa06
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-17 09:04:17 +0000

    [LeetCode Sync] Runtime - 70 ms (6.75%), Memory - 14 MB (100.00%)

--- 0692-top-k-frequent-words/solution.py
+++ 0692-top-k-frequent-words/solution.py
@@ -1,11 +1,4 @@
-from collections import Counter
-
-
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counts = Counter(words)
-        counts = [(k, v) for k,v in counts.most_common()]
-        counts.sort(key=lambda s:s[0])
-        counts.sort(key=lambda s:s[1], reverse=True)
-        return [k for k,v in counts][0:k]
-        
+        return [x[0] for x in sorted(counts.items(), key=lambda x: (-x[1], x[0]))][:k]
