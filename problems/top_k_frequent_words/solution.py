f864c15d55dbd8b15a76c41ffeeaa0148e1c94cc
Vishrut Jha <vishrutkmr7@gmail.com>
2022-10-17 09:04:17 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- problems/top_k_frequent_words/solution.py
+++ problems/top_k_frequent_words/solution.py
@@ -1,11 +1,4 @@
-from collections import Counter
-
-
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counts = Counter(words)
-        counts = list(counts.most_common())
-        counts.sort(key=lambda s:s[0])
-        counts.sort(key=lambda s:s[1], reverse=True)
-        return [k for k,v in counts][:k]
-        
+        return [x[0] for x in sorted(counts.items(), key=lambda x: (-x[1], x[0]))][:k]
