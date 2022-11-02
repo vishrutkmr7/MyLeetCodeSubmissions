d507ac73c13823fef2eb17aba37ca0f11df56c05
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-02 21:24:26 +0000

    Sync LeetCode submission - Minimum Genetic Mutation (python3)

---
+++ problems/minimum_genetic_mutation/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: list[str]) -> int:
+        def checkNeighbor(a, b):
+            return sum(a[i] != b[i] for i in range(len(a))) == 1
+
+        q = deque([start])
+        visited = {start}
+        mutations = 0
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur == end:
+                    return mutations
+                for neighbor in bank:
+                    if checkNeighbor(cur, neighbor) and neighbor not in visited:
+                        visited.add(neighbor)
+                        q.append(neighbor)
+            mutations += 1
+        return -1
