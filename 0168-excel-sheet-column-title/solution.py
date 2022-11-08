5c7e1bde310e5a2673e3d836dd0fec6c60200203
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-08 16:00:16 +0000

    [LeetCode Sync] Runtime - 22 ms (99.32%), Memory - 13.8 MB (100.00%)

---
+++ 0168-excel-sheet-column-title/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        result = ""
+        while n > 0:
+            n -= 1
+            result = chr(n % 26 + ord("A")) + result
+            n //= 26
+        return result
