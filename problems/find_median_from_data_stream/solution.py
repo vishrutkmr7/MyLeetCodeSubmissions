728e066be7cb723b25ee72da5bda3646ba142f69
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-12 23:11:20 +0000

    Sync LeetCode submission - Find Median from Data Stream (python3)

---
+++ problems/find_median_from_data_stream/solution.py
@@ -0,0 +1,26 @@
+from sortedcontainers import SortedList
+
+
+class MedianFinder(object):
+    def __init__(self):
+        self.data = SortedList()
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        self.data.add(num)
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        n = len(self.data)
+        return (self.data[n//2] + self.data[(n-1)//2]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
