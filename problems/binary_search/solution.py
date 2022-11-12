8a814fa40a99d765f1ebdcba8956db7a2d951518
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-12 23:01:27 +0000

    Sync LeetCode submission - Binary Search (python)

--- problems/binary_search/solution.py
+++ problems/binary_search/solution.py
@@ -1,13 +1,24 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        low = 0
-        high = len(nums) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return -1
+def binary_search(nums, target):
+    """Searches for target within nums"""
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return binary_search(nums, target)
