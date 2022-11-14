2f5ee56b08f86e7aeef161d24370db08814d514e
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-14 22:43:33 +0000

    Sync LeetCode submission - Most Stones Removed with Same Row or Column (python3)

--- problems/most_stones_removed_with_same_row_or_column/solution.py
+++ problems/most_stones_removed_with_same_row_or_column/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
+        root = set()
         parent = {}
         rank = {}
         for x, y in stones:
             self.set_param(x, parent, rank)
             self.set_param(~y, parent, rank)
             self.union(x, ~y, parent, rank)
-
-        root = {self.find(value, parent) for value in parent.values()}
+            
+        for p in parent:
+            root.add(self.find(parent[p], parent))
+            
         return len(stones) - len(root)
         
     def set_param(self, x, parent, rank):
@@ -23,12 +26,12 @@ class Solution:
     def union(self, x, y, parent, rank):
         parent_x = self.find(x, parent)
         parent_y = self.find(y, parent)
-
+        
         if(parent_x == parent_y):
             return
         rank_x = rank[parent_x]
         rank_y = rank[parent_y]
-
+        
         if(rank_x > rank_y):
             parent[parent_y] = parent_x
         elif(rank_x < rank_y):
