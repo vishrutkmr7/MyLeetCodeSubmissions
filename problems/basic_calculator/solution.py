825874d69c4dc01da5e1ff1e7074dc00a3c22c49
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-20 01:14:39 +0000

    Sync LeetCode submission - Basic Calculator (python3)

---
+++ problems/basic_calculator/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        sign = 1
+        res = 0
+        num = 0
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c == "+":
+                res += sign * num
+                sign = 1
+                num = 0
+            elif c == "-":
+                res += sign * num
+                sign = -1
+                num = 0
+            elif c == "(":
+                stack.extend((res, sign))
+                sign = 1
+                res = 0
+            elif c == ")":
+                res += sign * num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + sign * num
