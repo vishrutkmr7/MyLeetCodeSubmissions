c22f6ed2cb2717a1d96aca2dcbd9b9740bd2eaaa
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-22 22:57:46 +0000

    Sync LeetCode submission - Divide Two Integers (python3)

---
+++ problems/divide_two_integers/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        # Constants.
+        MAX_INT = 2147483647        # 2**31 - 1
+        MIN_INT = -2147483648       # -2**31
+        HALF_MIN_INT = -1073741824
+
+        if dividend == MIN_INT and divisor == -1:
+            return MAX_INT
+
+        negatives = 2
+        if dividend > 0:
+            negatives -= 1
+            dividend = -dividend
+        if divisor > 0:
+            negatives -= 1
+            divisor = -divisor
+
+        max_bit = 0
+        while divisor >= HALF_MIN_INT and divisor + divisor >= dividend:
+            max_bit += 1
+            divisor += divisor
+
+        quotient = 0
+        for bit in range(max_bit, -1, -1):
+            if divisor >= dividend:
+                quotient -= (1 << bit)
+                dividend -= divisor
+            divisor = (divisor + 1) >> 1
+
+        return -quotient if negatives != 1 else quotient
