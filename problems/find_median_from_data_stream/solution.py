b5dacc6f0418dcd3286d48c7cfd181b6f026ed8d
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-23 23:50:06 +0000

    Sync LeetCode submission - Find Median from Data Stream (python3)

--- problems/find_median_from_data_stream/solution.py
+++ problems/find_median_from_data_stream/solution.py
@@ -1,26 +1,16 @@
-from sortedcontainers import SortedList
-
-
 class MedianFinder(object):
     def __init__(self):
-        self.data = SortedList()
+        self.minHeap = []
+        self.maxHeap = []
 
     def addNum(self, num):
-        """
-        :type num: int
-        :rtype: None
-        """
-        self.data.add(num)
+        if len(self.minHeap) == len(self.maxHeap):
+            heapq.heappush(self.maxHeap, -heapq.heappushpop(self.minHeap, num))
+        else:
+            heapq.heappush(self.minHeap, -heapq.heappushpop(self.maxHeap, -num))
 
     def findMedian(self):
-        """
-        :rtype: float
-        """
-        n = len(self.data)
-        return (self.data[n//2] + self.data[(n-1)//2]) / 2
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+        if len(self.minHeap) == len(self.maxHeap):
+            return (self.minHeap[0] - self.maxHeap[0]) / 2.0
+        else:
+            return -self.maxHeap[0]
