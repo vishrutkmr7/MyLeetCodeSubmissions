942d2e16c10bfd80c427b4baf06c4ddc7f017621
Vishrut Jha <vishrutkmr7@gmail.com>
2022-11-24 00:00:06 +0000

    Sync LeetCode submission - 3Sum Closest (python3)

--- problems/3sum_closest/solution.py
+++ problems/3sum_closest/solution.py
@@ -1,32 +1,18 @@
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        i, N = 0, len(nums)
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
         nums.sort()
-        m = math.inf
-        res = None
-        if len(nums) == 3:
-            return sum(nums)
-        if nums[0] + nums[1] + nums[2] >= target:
-            return nums[0] + nums[1] + nums[2]
-        if nums[-3] + nums[-2] + nums[-1] <= target:
-            return nums[-3] + nums[-2] + nums[-1]
-        for i in range(N - 2):
-
-            left = i + 1
-            right = N - 1
-
-            while left < right:
-                s = nums[i] + nums[left] + nums[right]
-
-                if abs(target - s) < m:
-                    m = abs(target - s)
-                    res = s
-
-                if s > target:
-                    right -= 1
-                elif s < target:
-                    left += 1
+        closest = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if abs(target - total) < abs(target - closest):
+                    closest = total
+                if total < target:
+                    j += 1
+                elif total > target:
+                    k -= 1
                 else:
-                    return target
-
-        return res
+                    return total
+        return closest
