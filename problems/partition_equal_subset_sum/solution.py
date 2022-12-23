13566673f79314eb7854f38ce3788cbf47718a6c
Vishrut Jha <vishrutkmr7@gmail.com>
2022-12-23 19:32:15 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (python3)

--- problems/partition_equal_subset_sum/solution.py
+++ problems/partition_equal_subset_sum/solution.py
@@ -1,21 +1,12 @@
 class Solution:
     def canPartition(self, nums: list[int]) -> bool:
-        if sum(nums) % 2 == 1:
+        total = sum(nums)
+        if total % 2 != 0:
             return False
-        memo = {}
-
-        def dfs(i, target):
-            if target == 0:
-                return True
-            if target < 0 or i == len(nums):
-                return False
-            if (i, target) in memo:
-                return memo[(i, target)]
-
-            if dfs(i + 1, target - nums[i]) or dfs(i + 1, target):
-                memo[(i, target)] = True
-                return True
-            memo[(i, target)] = False
-            return memo[(i, target)]
-
-        return dfs(0, sum(nums) // 2)
+        target = total // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        return dp[target]
