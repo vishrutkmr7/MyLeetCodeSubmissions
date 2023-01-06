fa87b559391204ddf523c5fb9bd786052ac7be1f
Vishrut Jha <vishrutkmr7@gmail.com>
2023-01-06 20:14:09 +0000

    Sync LeetCode submission - Count Binary Substrings (python3)

--- problems/count_binary_substrings/solution.py
+++ problems/count_binary_substrings/solution.py
@@ -1,4 +1,13 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        s = list(map(len, s.replace('01', '0 1').replace('10', '1 0').split()))
-        return sum(min(a, b) for a, b in zip(s, s[1:]))
+        count = 0
+        prev = 0
+        curr = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                curr += 1
+            else:
+                count += min(prev, curr)
+                prev = curr
+                curr = 1
+        return count + min(prev, curr)
