8713e5107e8d8bf319b35cd6de650d0143bee934
Vishrut Jha <vishrutkmr7@gmail.com>
2023-01-28 21:00:39 +0000

    Sync LeetCode submission - Basic Calculator II (python3)

--- problems/basic_calculator_ii/solution.py
+++ problems/basic_calculator_ii/solution.py
@@ -6,18 +6,20 @@ class Solution:
         for i,c in enumerate(s):
             if '0' <= c <= '9':
                 num += c
-
+            
             if c == '+' or c == '*' or c == '-' or c == '/' or i == len(s) - 1:
+                if operator == "":
+                    stack.append(int(num))     
+                if operator == '+': 
+                    stack.append(int(num))        
                 if operator == '*':
-                    stack.append(stack.pop()*int(num))
-                elif operator == '-':
+                    stack.append(stack.pop()*int(num))         
+                if operator == '-':
                     stack.append(-1*int(num))
-                elif operator == '/':
+                if operator == '/':
                     stack.append(int(stack.pop()/int(num)))
-
-                elif operator in ["", '+']:
-                    stack.append(int(num))
+                    
                 num = ""
                 operator = c
-
+        
         return sum(stack)
