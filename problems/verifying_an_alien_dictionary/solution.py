37e7cbbdbbfbf7739eb44eec4eb9a38845c7257e
Vishrut Jha <vishrutkmr7@gmail.com>
2023-02-02 01:36:36 +0000

    Sync LeetCode submission - Verifying an Alien Dictionary (python3)

--- problems/verifying_an_alien_dictionary/solution.py
+++ problems/verifying_an_alien_dictionary/solution.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isAlienSorted(self, words, order):
-        """
-        :type words: List[str]
-        :type order: str
-        :rtype: bool
-        """
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
         hmap = {c: i for i, c in enumerate(order)}
         words = [[hmap[c] for c in w] for w in words]
         return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
