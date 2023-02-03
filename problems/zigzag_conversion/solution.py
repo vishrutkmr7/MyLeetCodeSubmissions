fee170031df932c454d29acc399c8bab134c6d0a
Vishrut Jha <vishrutkmr7@gmail.com>
2023-02-03 00:06:43 +0000

    Sync LeetCode submission - Zigzag Conversion (python3)

--- problems/zigzag_conversion/solution.py
+++ problems/zigzag_conversion/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1: return s
+        if numRows == 1:
+            return s
         lines = [''] * numRows
-        period = 2*numRows - 2
+        period = 2 * numRows - 2
         half_period = period //2
         for i in range(len(s)):
             line_num = abs(abs((i % period) - half_period) - half_period)
