bcaf47c099584b6f1ecc097abe218d439e1a3960
Vishrut Jha <vishrutkmr7@gmail.com>
2023-02-26 13:57:20 +0000

    Sync LeetCode submission - Toeplitz Matrix (python3)

--- problems/toeplitz_matrix/solution.py
+++ problems/toeplitz_matrix/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        return all(r1[:-1] == r2[1:] for r1,r2 in zip(matrix, matrix[1:]))
+        return all(r1[:-1] == r2[1:] for r1, r2 in zip(matrix, matrix[1:]))
