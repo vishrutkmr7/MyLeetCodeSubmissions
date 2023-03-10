b1792f739f87989647f2b2adfe9ae47be5829a53
Vishrut Jha <vishrutkmr7@gmail.com>
2023-03-10 00:05:51 +0000

    Sync LeetCode submission - Linked List Random Node (python3)

---
+++ problems/linked_list_random_node/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+
+    def getRandom(self) -> int:
+        ans = 0
+        p, i = self.head, 0
+        while p:
+            if random.randint(0, i) == 0:
+                ans = p.val
+            p = p.next
+            i += 1
+
+        return ans
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
