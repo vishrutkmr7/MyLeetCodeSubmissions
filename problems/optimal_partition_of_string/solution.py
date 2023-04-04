7ba6219ec3620db340464a06b1244dffdb6c2011
Vishrut Jha <vishrutkmr7@gmail.com>
2023-04-04 00:28:51 +0000

    Sync LeetCode submission - Optimal Partition of String (python3)

---
+++ problems/optimal_partition_of_string/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        cur = set()
+        res = 1
+        
+        for c in s:
+            if c in cur:
+                cur = set()
+                res += 1
+            cur.add(c)
+                
+        return res
+        
