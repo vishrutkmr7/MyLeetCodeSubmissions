da1090044e5ebe2ec5cf10059dd98ad7d705addd
Vishrut Jha <vishrutkmr7@gmail.com>
2023-04-20 00:12:33 +0000

    Sync LeetCode submission - Maximum Width of Binary Tree (python3)

--- problems/maximum_width_of_binary_tree/solution.py
+++ problems/maximum_width_of_binary_tree/solution.py
@@ -1,23 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def widthOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if not root: return 0 #edge case 
-        ans = 0
-        queue = [(root, 0)]
-        while queue: 
-            ans = max(ans, queue[-1][1] - queue[0][1] + 1)
-            temp = []
-            for node, i in queue: 
-                if node.left: temp.append((node.left, 2*i))
-                if node.right: temp.append((node.right, 2*i+1))
-            queue = temp
-        return ans 
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        max_width = 0
+        queue = deque([(root, 0)])  # Each tuple contains a TreeNode and its position
+
+        while queue:
+            level_length = len(queue)
+            leftmost_pos = queue[0][1]  # Position of the leftmost node in the current level
+            rightmost_pos = leftmost_pos  # Initialize rightmost_pos with leftmost_pos
+
+            for _ in range(level_length):
+                node, pos = queue.popleft()
+
+                # Update rightmost_pos with the position of the current node
+                rightmost_pos = pos
+
+                if node.left:
+                    queue.append((node.left, pos * 2))
+                if node.right:
+                    queue.append((node.right, pos * 2 + 1))
+
+            # Update max_width by comparing it with the width of the current level
+            max_width = max(max_width, rightmost_pos - leftmost_pos + 1)
+
+        return max_width
