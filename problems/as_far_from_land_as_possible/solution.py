d7ba9c384a1d4fcf194a9eb331e782b4e9ade659
Vishrut Jha <vishrutkmr7@gmail.com>
2023-04-29 19:10:33 +0000

    Sync LeetCode submission - As Far from Land as Possible (python3)

--- problems/as_far_from_land_as_possible/solution.py
+++ problems/as_far_from_land_as_possible/solution.py
@@ -1,26 +1,19 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        dist = 0
-        q = deque()
-        drow = [0, -1, 0, 1]
-        dcol = [-1, 0, 1, 0]
-
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    q.append((i, j))
-        if len(q) == n * n:
+        if not grid:
             return -1
-
+        m, n = len(grid), len(grid[0])
+        q = deque([(i, j) for i in range(m) for j in range(n) if grid[i][j] == 1])
+        if len(q) in [0, m * n]:
+            return -1
+        level = 0
         while q:
-            size = len(q)
-            dist += 1
-            for _ in range(size):
-                r, c = q.popleft()
-                for i in range(4):
-                    nrow, ncol = r + drow[i], c + dcol[i]
-                    if 0 <= nrow < n and 0 <= ncol < n and grid[nrow][ncol] == 0:
-                        grid[nrow][ncol] = 1
-                        q.append((nrow, ncol))
-        return dist - 1
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    new_i, new_j = i + x, j + y
+                    if 0 <= new_i < m and 0 <= new_j < n and grid[new_i][new_j] == 0:
+                        grid[new_i][new_j] = 1
+                        q.append((new_i, new_j))
+            level += 1
+        return level - 1
