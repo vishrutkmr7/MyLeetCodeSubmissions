e09c7f66e8d2dabc9cbce3b3aec07597c0e7312e
Vishrut Jha <vishrutkmr7@gmail.com>
2023-05-01 08:46:08 +0000

    Sync LeetCode submission - Break a Palindrome (python3)

--- problems/break_a_palindrome/solution.py
+++ problems/break_a_palindrome/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        if(len(palindrome)==1):
+        if len(palindrome) == 1:
             return ""
         return next(
             (
-                f'{palindrome[:i]}a{palindrome[i + 1:]}'
+                f"{palindrome[:i]}a{palindrome[i + 1:]}"
                 for i, c in enumerate(palindrome)
-                if (c != 'a' and i * 2 + 1 != len(palindrome))
+                if (c != "a" and i * 2 + 1 != len(palindrome))
             ),
-            f'{palindrome[:-1]}b',
+            f"{palindrome[:-1]}b",
         )
