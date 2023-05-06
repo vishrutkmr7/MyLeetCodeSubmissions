805ec7a26244d2b1a3dde181d45cea48f75ad3fb
Vishrut Jha <vishrutkmr7@gmail.com>
2023-05-06 22:57:17 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -1,31 +1,16 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        def helper(cur):
-            if len(cur) == 0: 
-                return ''
-
-            i, num, prefix, level, depth, start = 0, '', '', 0, 0, math.inf
-
-            while i < len(cur) and 97 <= ord(cur[i]) <= 122: # letter 
-                prefix += cur[i]
-                i += 1
-            while i < len(cur) and 48 <= ord(cur[i]) <= 58: # num
-                num += cur[i]
-                i += 1
-
-            while i < len(cur): 
-                if cur[i] == '[': 
-                    level += 1 # tracks how many opening bracket I have 
-                    depth = max(depth, level) # tracks how far I ever reached 
-                    start = min(start, i + 1) # tracks where first opening bracket was 
-                elif cur[i] == ']': 
-                    level -= 1
-                    if level == 0: 
-                        if depth > 1: 
-                            return prefix + helper(cur[start:i]) * int(num) + helper(cur[i+1:])
-                        else: 
-                            return prefix + cur[start:i] * int(num) + helper(cur[i+1:])
-                i += 1
-            return prefix
-
-        return helper(s)
+        stack = []
+        for char in s:
+            if char != "]":
+                stack.append(char)
+            else:
+                temp = ""
+                while stack[-1] != "[":
+                    temp = stack.pop() + temp
+                stack.pop()
+                num = ""
+                while stack and stack[-1].isdigit():
+                    num = stack.pop() + num
+                stack.append(int(num) * temp)
+        return "".join(stack)
