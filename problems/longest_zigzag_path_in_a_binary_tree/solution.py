df296b48baabd7246bdc21fd98ab09dc7286cfe2
Vishrut Jha <vishrutkmr7@gmail.com>
2023-05-08 21:23:01 +0000

    Sync LeetCode submission - Longest ZigZag Path in a Binary Tree (python3)

--- problems/longest_zigzag_path_in_a_binary_tree/solution.py
+++ problems/longest_zigzag_path_in_a_binary_tree/solution.py
@@ -7,8 +7,13 @@
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         def dfs(root):
-            if not root: return [-1, -1, -1]
+            if not root:
+                return [-1, -1, -1]
             left, right = dfs(root.left), dfs(root.right)
-            return [left[1] + 1, right[0] + 1, max(left[1] + 1, right[0] + 1, left[2], right[2])]
-            
+            return [
+                left[1] + 1,
+                right[0] + 1,
+                max(left[1] + 1, right[0] + 1, left[2], right[2]),
+            ]
+
         return dfs(root)[-1]
