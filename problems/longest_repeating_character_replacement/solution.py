8b7ee6437b98e158371bbc242dfa927b9d165884
Vishrut Jha <vishrutkmr7@gmail.com>
2023-05-10 19:49:12 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python3)

--- problems/longest_repeating_character_replacement/solution.py
+++ problems/longest_repeating_character_replacement/solution.py
@@ -4,8 +4,8 @@ class Solution:
         left = 0
         result = 0
 
-        for right in range(len(s)):
-            hashMap[s[right]] += 1
+        for right, value in enumerate(s):
+            hashMap[value] += 1
 
             while right - left + 1 - max(hashMap.values()) > k:
                 hashMap[s[left]] -= 1
