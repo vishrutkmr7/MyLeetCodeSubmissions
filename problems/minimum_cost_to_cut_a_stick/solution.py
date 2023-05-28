93d707b95634203ac2f1c84d45d2533b37e80bc6
Vishrut Jha <vishrutkmr7@gmail.com>
2023-05-28 09:35:52 +0000

    Sync LeetCode submission - Minimum Cost to Cut a Stick (python3)

---
+++ problems/minimum_cost_to_cut_a_stick/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts = sorted(cuts + [0, n])
+        k = len(cuts)
+        dp = [[0] * k for _ in range(k)]
+        for d in range(2, k):
+            for i in range(k - d):
+                dp[i][i + d] = (
+                    min(dp[i][m] + dp[m][i + d] for m in range(i + 1, i + d))
+                    + cuts[i + d]
+                    - cuts[i]
+                )
+        return dp[0][k - 1]
