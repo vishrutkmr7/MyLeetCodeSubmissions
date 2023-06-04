6f20380a4def6d42c6c7538610c162625e072cab
Vishrut Jha <vishrutkmr7@gmail.com>
2023-06-04 00:19:17 +0000

    Sync LeetCode submission - Number of Provinces (python3)

--- problems/number_of_provinces/solution.py
+++ problems/number_of_provinces/solution.py
@@ -1,16 +1,22 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        array = [-1 for _ in range(len(isConnected))]
+        array = [-1 for i in range(len(isConnected))]
         for i in range(len(array)):
             for j in range(len(array)):
                 if i != j and isConnected[i][j] == 1:
                     self.union(array,i,j)
-        return sum(array[i] == -1 for i in range(len(array)))
+        count=0
+        for i in range(len(array)):
+            if array[i] == -1:
+                count += 1
+        return count
                     
         
         
     def find(self, array, index):
-        return index if array[index] == -1 else self.find(array,array[index])
+        if array[index] == -1:
+            return index
+        return self.find(array,array[index])
 
     def union(self, array, index1, index2):
         root1 = self.find(array, index1)
