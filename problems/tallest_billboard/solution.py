98bd18d38ea22961510a266887e7bb330235fa90
Vishrut Jha <vishrutkmr7@gmail.com>
2023-06-24 00:07:32 +0000

    Sync LeetCode submission - Tallest Billboard (python3)

---
+++ problems/tallest_billboard/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tallestBillboard(self, rods: List[int]) -> int:
+        dp = defaultdict(int)
+        dp[0] = 0
+        for x in rods:
+            nxt = dp.copy()
+            for d, y in dp.items():
+                nxt[d + x] = max(nxt.get(x + d, 0), y)
+                nxt[abs(d - x)] = max(nxt.get(abs(d - x), 0), y + min(d, x))
+            dp = nxt
+        return dp[0]
