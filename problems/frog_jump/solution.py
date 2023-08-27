c1bcd79b775ac14cce4fb44b5fa0ac6cda3d0665
Vishrut Jha <vishrutkmr7@gmail.com>
2023-08-27 01:46:22 +0000

    Sync LeetCode submission - Frog Jump (python3)

--- problems/frog_jump/solution.py
+++ problems/frog_jump/solution.py
@@ -1,18 +1,20 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        a=set(stones)
-        if stones[1]!=1:
-            return False
-        @cache
-        def dfs(last,pos):
-            if pos==stones[-1]:
-                return True
-            if last==pos:
-                return False
-            jump=pos-last
-            for x in [pos+jump-1,pos+jump,pos+jump+1]:
-                if x in a and dfs(pos, x):
-                    return True
+        a = set(stones)
+        if stones[1]!= 1:
             return False
 
-        return dfs(0,1)
+        @cache
+        def dfs(last,pos):
+            if pos == stones[-1]:
+                return True
+            if last == pos:
+                return False
+            jump = pos - last
+            for x in [pos + jump - 1, pos + jump, pos + jump + 1]:
+                if x in a:
+                    if dfs(pos, x):
+                        return True
+            return False
+        
+        return dfs(0, 1)
