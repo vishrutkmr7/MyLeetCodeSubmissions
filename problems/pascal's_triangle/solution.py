d385f457e19159f75d537a12a09cef36304ef6fe
Vishrut Jha <vishrutkmr7@gmail.com>
2023-09-08 00:06:43 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

---
+++ problems/pascal's_triangle/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        output = []
+        for i in range(numRows):
+            if(i == 0):
+                prev = [1]
+                output.append(prev)
+            else:
+                curr = [1]
+                j = 1
+                while(j < i):
+                    curr.append(prev[j - 1] + prev[j])
+                    j+=1
+                curr.append(1)
+                output.append(curr)
+                prev = curr
+        return output  
+        
