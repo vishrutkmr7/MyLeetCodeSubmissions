bdabe63a395976687a7194f22d3586923d75c159
Vishrut Jha <vishrutkmr7@gmail.com>
2023-09-28 00:29:26 +0000

    Sync LeetCode submission - Sort Array By Parity (python3)

---
+++ problems/sort_array_by_parity/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        beg, end = 0, len(nums) - 1
+        
+        while beg <= end:
+            if nums[beg] % 2 == 0:
+                beg += 1
+            else:
+                nums[beg], nums[end] = nums[end], nums[beg]
+                end -= 1
+        return nums
