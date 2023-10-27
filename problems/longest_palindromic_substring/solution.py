9d6480191f056252bb5d3c6885739d24da161df5
Vishrut Jha <vishrutkmr7@gmail.com>
2023-10-27 00:05:59 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -6,9 +6,10 @@ class Solution:
         dp = [[ i == j for i in range(n)] for j in range(n)]
         for i in range(n):
             for j in range(i):
-                if s[i] == s[j] and (j + 1 == i or dp[j + 1][i - 1]):
-                    dp[j][i] = True
-                    if i - j + 1 >= ans_l:
-                        ans_l = i - j + 1
-                        ans = s[j: i+1]
+                if s[i] == s[j]:
+                    if j + 1 == i or dp[j+1][i-1]:
+                        dp[j][i] = True
+                        if i - j + 1 >= ans_l:
+                            ans_l = i - j + 1
+                            ans = s[j: i+1]
         return ans
